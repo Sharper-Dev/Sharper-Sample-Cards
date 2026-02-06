7244b8337d707902588693b1e7073df7420b79fd
Guilherme Sena <guilherme.senadeaguiar626@gmail.com>
2026-02-05 23:51:00 -0300

    Add simple template

---
+++ simple_card_template.lua
@@ -0,0 +1,24 @@
+-- This template includes the basic structure of a card script
+function s.initial_effect(c)
+
+    local e1 = Effect.CreateEffect(c)
+
+    e1:SetDescription(aux.Stringid(id, 0))
+    e1:SetCategory()
+    e1:SetType()
+    e1:SetProperty()
+    e1:SetCode()
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+
+    c:RegisterEffect(e1)
+
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+   
+end
